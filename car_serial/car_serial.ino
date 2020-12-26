3376c03781ebe55e424b90039a20221f00b44ee7
kakuto <umihei2h@gmail.com>
2020-12-26 15:58:48 +0900

    first commit. ROSを用いた簡単なラジコンプログラム。

---
+++ car_serial/car_serial.ino
@@ -0,0 +1,122 @@
+#include <Servo.h>
+#include <ros.h>
+#include <std_msgs/Int32.h>
+
+#define REAR_LED_L 8
+#define REAR_LED_R 9
+#define FRONT_LED_L 10
+#define FRONT_LED_R 11
+#define SERVO_PIN 6
+#define MOTOR_SIG1 3
+#define MOTOR_SIG2 4
+#define ENABLE 5
+
+Servo myServo;
+ros::NodeHandle nh;
+bool ledControlleFlag = true;
+
+void messageCb_led(const std_msgs::Int32 &led_msg){
+  if(led_msg.data){
+    ledControlleFlag=false;
+    digitalWrite(REAR_LED_L, HIGH);
+    digitalWrite(REAR_LED_R, HIGH);
+    digitalWrite(FRONT_LED_L, HIGH);
+    digitalWrite(FRONT_LED_R, HIGH);
+  }else{
+    ledControlleFlag=true;
+    digitalWrite(REAR_LED_L, LOW);
+    digitalWrite(REAR_LED_R, LOW);
+    digitalWrite(FRONT_LED_L, LOW);
+    digitalWrite(FRONT_LED_R, LOW);
+  }
+}
+
+void messageCb_servo(const std_msgs::Int32 &servo_msg){
+  if(65<=servo_msg.data && servo_msg.data<=115){
+    myServo.write(servo_msg.data);
+
+    if(ledControlleFlag){
+      if(servo_msg.data<90) {
+        digitalWrite(REAR_LED_L, HIGH);
+        digitalWrite(REAR_LED_R, LOW); 
+      }else if(90<servo_msg.data) {
+        digitalWrite(REAR_LED_R, HIGH);
+        digitalWrite(REAR_LED_L, LOW); 
+      }else {
+        digitalWrite(REAR_LED_L, LOW); 
+        digitalWrite(REAR_LED_R, LOW);
+      }
+    }
+  }
+}
+
+void messageCb_motor(const std_msgs::Int32 &motor_msg){
+  if(motor_msg.data == 1){
+    // 前進
+    digitalWrite(MOTOR_SIG1, HIGH);
+    digitalWrite(MOTOR_SIG2, LOW);
+    digitalWrite(ENABLE, HIGH);
+    // 前LED点灯
+    if(ledControlleFlag){
+      digitalWrite(FRONT_LED_L, HIGH);
+      digitalWrite(FRONT_LED_R, HIGH);
+    }
+  }else if(motor_msg.data == -1){
+    // 後退
+    digitalWrite(MOTOR_SIG1, LOW);
+    digitalWrite(MOTOR_SIG2, HIGH);
+    digitalWrite(ENABLE, HIGH);
+    // 前LED消灯
+    if(ledControlleFlag){
+      digitalWrite(FRONT_LED_L, LOW);
+      digitalWrite(FRONT_LED_R, LOW);
+    }
+    
+  }else{
+    // ストップ
+    digitalWrite(MOTOR_SIG1, LOW);
+    digitalWrite(MOTOR_SIG2, LOW);
+    digitalWrite(ENABLE, LOW);
+    // 前LED消灯
+    if(ledControlleFlag){
+      digitalWrite(FRONT_LED_L, LOW);
+      digitalWrite(FRONT_LED_R, LOW);
+    }
+  }
+}
+
+// コールバック関数
+ros::Subscriber<std_msgs::Int32> sub_led("led", &messageCb_led);
+ros::Subscriber<std_msgs::Int32> sub_servo("servo", &messageCb_servo);
+ros::Subscriber<std_msgs::Int32> sub_motor("motor", &messageCb_motor);
+
+void setup() {
+  // LED
+  pinMode(REAR_LED_L,  OUTPUT);
+  pinMode(REAR_LED_R,  OUTPUT);
+  pinMode(FRONT_LED_L,  OUTPUT);
+  pinMode(FRONT_LED_R,  OUTPUT);
+
+  // サーボ
+  myServo.attach(SERVO_PIN);
+  myServo.write(90);
+
+  // DCモーター
+  pinMode(MOTOR_SIG1, OUTPUT);
+  pinMode(MOTOR_SIG2, OUTPUT);
+  pinMode(ENABLE, OUTPUT);
+
+  // ROS初期設定
+  nh.getHardware()->setBaud(115200);
+  nh.initNode();
+
+  // コールバック関数
+  nh.subscribe(sub_led);
+  nh.subscribe(sub_servo);
+  nh.subscribe(sub_motor);
+}
+
+void loop() {
+  nh.spinOnce();
+  delay(1);
+}
---
+++ src/ros_car.cpp
@@ -0,0 +1,37 @@
+#include <ros/ros.h>
+#include <std_msgs/Int32.h>
+#include <sensor_msgs/Joy.h>
+
+std_msgs::Int32 led_msg;
+std_msgs::Int32 servo_msg;
+std_msgs::Int32 motor_msg;
+
+void joy_callback(const sensor_msgs::Joy joy_msg){
+  led_msg.data = joy_msg.buttons[0];
+  servo_msg.data = -joy_msg.axes[2]*25+90;
+
+  if(joy_msg.axes[1] > 0.5) motor_msg.data = 1;
+  else if(joy_msg.axes[1] < -0.5) motor_msg.data = -1;
+  else motor_msg.data = 0;
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "ros_car_node");
+  ros::NodeHandle nh;
+  ros::Subscriber sub = nh.subscribe("joy", 10, joy_callback);
+  ros::Publisher pub_led = nh.advertise<std_msgs::Int32>("led", 10);
+  ros::Publisher pub_servo = nh.advertise<std_msgs::Int32>("servo", 10);
+  ros::Publisher pub_motor = nh.advertise<std_msgs::Int32>("motor", 10);
+  ros::Rate loop_rate(30);
+
+  while (ros::ok())
+  {
+    pub_led.publish(led_msg);
+    pub_servo.publish(servo_msg);
+    pub_motor.publish(motor_msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
