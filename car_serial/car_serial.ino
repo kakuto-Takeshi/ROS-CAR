6ebae33813d75ca55959ea5f80cb2df96aa351b9
kakuto <umihei2h@gmail.com>
2021-08-14 19:15:09 +0900

    モーター動作を左右独立。前進、後退、旋回が可能。

--- car_serial/car_serial.ino
+++ car_serial/car_serial.ino
@@ -1,62 +1,98 @@
-#include <Servo.h>
 #include <ros.h>
 #include <std_msgs/Int32.h>
 
-#define SERVO_PIN 6
-#define MOTOR_SIG1 3
-#define MOTOR_SIG2 4
-#define ENABLE 5
+#define ENABLE_L 2
+#define MOTOR_L_SIG1 3
+#define MOTOR_L_SIG2 4
+#define ENABLE_R 5
+#define MOTOR_R_SIG1 6
+#define MOTOR_R_SIG2 7
 
-Servo myServo;
-const int OFFSET = 7;
 ros::NodeHandle nh;
 
-void messageCb_servo(const std_msgs::Int32 &servo_msg){
-  if(65<=servo_msg.data && servo_msg.data<=115){
-    myServo.write(servo_msg.data+OFFSET);
+void setMotor(int LR, char mode){
+  switch(mode){
+    case 'f' :
+      digitalWrite(LR+1, HIGH);
+      digitalWrite(LR+2, LOW);
+      break;
+    case 'n' :
+      digitalWrite(LR+1, LOW);
+      digitalWrite(LR+2, LOW);
+      break;
+    case 'r' :
+      digitalWrite(LR+1, LOW);
+      digitalWrite(LR+2, HIGH);
+      break;
   }
 }
 
 void messageCb_motor(const std_msgs::Int32 &motor_msg){
-  if(motor_msg.data == 1){
-    // 前進
-    digitalWrite(MOTOR_SIG1, HIGH);
-    digitalWrite(MOTOR_SIG2, LOW);
-    digitalWrite(ENABLE, HIGH);
-  }else if(motor_msg.data == -1){
-    // 後退
-    digitalWrite(MOTOR_SIG1, LOW);
-    digitalWrite(MOTOR_SIG2, HIGH);
-    digitalWrite(ENABLE, HIGH);
-  }else{
-    // ストップ
-    digitalWrite(MOTOR_SIG1, LOW);
-    digitalWrite(MOTOR_SIG2, LOW);
-    digitalWrite(ENABLE, LOW);
+
+  switch(motor_msg.data){
+    case 0 :
+      setMotor(ENABLE_L, 'n');
+      setMotor(ENABLE_R, 'f');
+      break;
+    case 1 :
+      setMotor(ENABLE_L, 'f');
+      setMotor(ENABLE_R, 'f');
+      break;
+    case 2 :
+      setMotor(ENABLE_L, 'f');
+      setMotor(ENABLE_R, 'n');
+      break;
+    case 3 :
+      setMotor(ENABLE_L, 'r');
+      setMotor(ENABLE_R, 'f');
+      break;
+    case 4 :
+      setMotor(ENABLE_L, 'n');
+      setMotor(ENABLE_R, 'n');
+      break;
+    case 5 :
+      setMotor(ENABLE_L, 'f');
+      setMotor(ENABLE_R, 'r');
+      break;
+    case 6 :
+      setMotor(ENABLE_L, 'n');
+      setMotor(ENABLE_R, 'r');
+      break;
+    case 7 :
+      setMotor(ENABLE_L, 'r');
+      setMotor(ENABLE_R, 'r');
+      break;
+    case 8 :
+      setMotor(ENABLE_L, 'r');
+      setMotor(ENABLE_R, 'n');
+      break;
+    default:
+      setMotor(ENABLE_L, 'n');
+      setMotor(ENABLE_R, 'n');
+      break;
   }
 }
 
 // サブスクライバー
-ros::Subscriber<std_msgs::Int32> sub_servo("servo", &messageCb_servo);
 ros::Subscriber<std_msgs::Int32> sub_motor("motor", &messageCb_motor);
 
 void setup() {
 
-  // サーボ
-  myServo.attach(SERVO_PIN);
-  myServo.write(90+OFFSET);
-
   // DCモーター
-  pinMode(MOTOR_SIG1, OUTPUT);
-  pinMode(MOTOR_SIG2, OUTPUT);
-  pinMode(ENABLE, OUTPUT);
+  pinMode(ENABLE_L, OUTPUT);
+  pinMode(MOTOR_L_SIG1, OUTPUT);
+  pinMode(MOTOR_L_SIG2, OUTPUT);
+  pinMode(ENABLE_R, OUTPUT);
+  pinMode(MOTOR_R_SIG1, OUTPUT);
+  pinMode(MOTOR_R_SIG2, OUTPUT);
+  digitalWrite(ENABLE_L, HIGH);
+  digitalWrite(ENABLE_R, HIGH);
 
   // ROS初期設定
   nh.getHardware()->setBaud(115200);
   nh.initNode();
 
   // サブスクライブ実行
-  nh.subscribe(sub_servo);
   nh.subscribe(sub_motor);
 }
 
